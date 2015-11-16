3efa2a9697dd7e857a63d084680c742d4c6835d8
Wojciech Baran <baran.wojciech@gmail.com>
2015-11-16 13:50:13 +0100

    Add views to functions in adm menu and small menu

--- js/adm.ctrl.js
+++ js/adm.ctrl.js
@@ -1,8 +1,11 @@
 testApp.controller('admController', function($scope, CONFIG, Search, AuthenticationService) {
   $scope.config = CONFIG;
-
+  $scope.tab = 'start';
   AuthenticationService.AllowAdmin();
 
+  $scope.setTab = function(tab) {
+    $scope.tab = tab;
+  };
   // $scope.search = function() {
   //   Search.search($scope.ww, 'ttt', function(response) {
   //   	$scope.response = response;
