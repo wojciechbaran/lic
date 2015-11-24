70a0f05fe102fb918070bc98b2b668f33d15273d
Wojciech Baran <baran.wojciech@gmail.com>
2015-11-24 14:43:32 +0100

    fix multilanguage directive part 2

--- js/multilang.js
+++ js/multilang.js
@@ -2,27 +2,41 @@ testApp.directive('ngMultilang', function(CONFIG) {
   function link(scope) {
     scope.languages = CONFIG.languages;
     scope.tab = CONFIG.defaultLanguage;
-    scope.val = JSON.parse(scope.val);
-    console.log(scope.val);
+    scope.eval = [];
     scope.setTab = function(tab) {
       scope.tab = tab;
     };
+    scope.setEval = function() {
+      if (scope.val && typeof(scope.val) == 'string') {
+        scope.val = JSON.parse(scope.val);
+      }
+      if (scope.val && scope.val.pl) {
+        scope.eval['pl'] = scope.val.pl;
+      } else {
+        scope.eval['pl'] = '';
+      }
+      if (scope.val && scope.val.en) {
+        scope.eval['en'] = scope.val.en;
+      } else {
+        scope.eval['en'] = '';
+      }
+    }
     scope.formMlS = function() {
-       scope.error = '';
-       scope.success = '';
+      scope.error = '';
+      scope.success = '';
       //$scope.dataLoading = true;
-      var data = scope.val;
-        // CUDService.Go('add', data, 'projects', 'name', function(response) {
-        //   if (response.success) {
-        //     $scope.editProject(response.newid);
-        //   } else {
-        //     $scope.newProject.error = response.message;
-        //   }
-        //   $scope.dataLoading = false;
-        // });
+      var data = scope.eval;
+      // CUDService.Go('add', data, 'projects', 'name', function(response) {
+      //   if (response.success) {
+      //     $scope.editProject(response.newid);
+      //   } else {
+      //     $scope.newProject.error = response.message;
+      //   }
+      //   $scope.dataLoading = false;
+      // });
       console.log(data);
-    };
-
+    }
+    scope.$watch('val', scope.setEval);
   }
   return {
     restrict: 'A',
