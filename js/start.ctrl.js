75e5384914f68fd8fbbf1074c31c51830cd45eaf
Wojciech Baran <baran.wojciech@gmail.com>
2016-02-20 00:20:27 +0100

    add list of new and old projects in start view

--- js/start.ctrl.js
+++ js/start.ctrl.js
@@ -1,7 +1,10 @@
 testApp.controller('startController', function($scope, CONFIG, SearchService) {
-  var query = {projectStatus:0 };
+  var query = {$or: [{projectStatus:1},{projectStatus:2}]};
   SearchService.search('advenced', query, 'projects', '', function(response) {
-    console.log(response);
+  	$scope.newProjects = response;
   });
-  
+  query = {$or: [{projectStatus:3},{projectStatus:4}]};
+  SearchService.search('advenced', query, 'projects', '', function(response) {
+  	$scope.oldProjects = response;
+  });
 });
