c90ec6dd2f0b79650e0e9dd7580bffe5a6a4dd4d
Wojciech Baran <baran.wojciech@gmail.com>
2016-05-08 17:22:44 +0200

    fix changing user to lecturer

--- js/lecturer.ctrl.js
+++ js/lecturer.ctrl.js
@@ -3,7 +3,19 @@ testApp.controller('lecturerController', function($scope, CONFIG, $rootScope, $l
   $scope.currentUser = $rootScope.currentUser;
 
   $scope.upgradeUser = function(){
-    CUDService.Go('update', $scope.currentUser, 'users', $scope.currentUser.id, function(response) {
+  	var data=[];
+  	$scope.config.userDataFilds.forEach(function(userDataFild) {
+  		var t={};
+  		t[userDataFild.name]=$scope.currentUser[userDataFild.name];
+  		data.push(t);
+	});
+	$scope.config.lecturerDataFilds.forEach(function(userDataFild) {
+  		var t={};
+  		t[userDataFild.name]=$scope.currentUser[userDataFild.name];
+  		data.push(t);
+	});
+  	console.log(data);
+    CUDService.Go('update', data, 'users', $scope.currentUser.id, function(response) {
       if (response.success) {
         $location.path('/' + CONFIG.route + '/user');
       }
