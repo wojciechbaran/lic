060c10966765c5b1e75a90b2972ebecb7d496949
Wojciech Baran <baran.wojciech@gmail.com>
2016-05-22 17:44:29 +0200

    fix articles full name directive

--- js/articlesfull.js
+++ js/articlesfull.js
@@ -1,12 +1,21 @@
-testApp.directive('ngArticlesfull', function(CONFIG) {
+testApp.directive('ngArticlesfull', function(CONFIG,SearchService) {
   function link(scope) {
-
-  	if(scope.user){
-      //console.log(scope.articlelink);
-      var len = CONFIG.fileUploadPath.length+1;
-      scope.userId=scope.articlelink.substring(len,len+13);
-      //console.log(scope.userId);
-    }    
+    scope.config = CONFIG;
+    scope.title={};
+    if(scope.cilckable){
+      scope.cilckablelink=scope.config.route+'/'+scope.articlelink;
+    }
+    var len = CONFIG.fileUploadPath.length+1;
+    scope.userId=scope.articlelink.substring(len,len+13);
+    SearchService.search('simple', 'id:'+scope.userId, 'users', 'id:ASC', function(response) {
+      scope.articles = response[0].articles;
+      for(var i = 0; i < scope.articles.length; i++){
+        if(scope.articles[i].file.link == scope.articlelink){
+          scope.title=scope.articles[i].title;
+        }
+      }
+      console.log(scope.title); 
+    });    
   }
   return {
     restrict: 'A',
@@ -14,7 +23,7 @@ testApp.directive('ngArticlesfull', function(CONFIG) {
       articlelink: '=',
       articlelist: '=',
       user: '@',
-      clickable: '@'
+      cilckable: '@'
     },
     templateUrl: CONFIG.route + '/views/articlesfull.html',
     link: link
